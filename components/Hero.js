45963b1ed53ac631a957e96201a5df5e35cd399d
wahyubudii <79882296+wahyubudii@users.noreply.github.com>
2022-02-06 19:03:00 +0700

    profile done

--- components/Hero.js
+++ components/Hero.js
@@ -1,30 +1,30 @@
-import Button from "./Button"
-import NavBar from "./Navbar"
+import Button from "./Button";
+import NavBar from "./NavBar";
 
 export default function Hero() {
-    return(
-        <div>
-        <div className="bg-hero h-[712px]">
-          <div className="container mx-auto">
-            <NavBar />
-            
-            {/* Hero */}
-            <div className="text-center mt-20">
-              <h1 className="text-3xl text-white font-semibold font-mono w-8/12 mx-auto leading-relaxed">
-                Saya seorang front-end engineer, back-end engineer, dan juga UI
-                designer
-              </h1>
-              <p className="text-white text-opacity-60 text-lg w-4/12 mx-auto mt-6 leading-relaxed">
-                Sejak 13 tahun saya memulai programming. Sejak 3 tahun memulai UI
-                design
-              </p>
-  
-              <Button className="mt-14" variant="yellow">
-                Pelajari
-              </Button>
-            </div>
+  return (
+    <div>
+      <div className="bg-hero h-[712px]">
+        <div className="container mx-auto">
+          <NavBar />
+
+          {/* Hero */}
+          <div className="text-center mt-20">
+            <h1 className="text-3xl text-white font-semibold font-mono w-8/12 mx-auto leading-relaxed">
+              Saya seorang front-end engineer, back-end engineer, dan juga UI
+              designer
+            </h1>
+            <p className="text-white text-opacity-60 text-lg w-4/12 mx-auto mt-6 leading-relaxed">
+              Sejak 13 tahun saya memulai programming. Sejak 3 tahun memulai UI
+              design
+            </p>
+
+            <Button className="mt-14" variant="yellow">
+              Pelajari
+            </Button>
           </div>
         </div>
       </div>
-    )
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,47 @@
 import Button from "../components/Button";
 import Hero from "../components/Hero";
-import NavBar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <Hero />
+    <div>
+      <Hero />
+      <section className="py-28">
+        <div className="container mx-auto">
+          <h2 className="text-2xl text-center font-mono font-semibold">
+            Profile
+          </h2>
+          <p className="text-lg text-center text-gray-500 mt-2">
+            Mengenal saya secara singkat.
+          </p>
+          <div className="mt-16 flex mx-auto w-10/12 item-center">
+            <div className="w-5/12 px-10">
+              <img src="/profile.png" className="w-full" />
+            </div>
+            <div className="w-7/12 px-10">
+              <p className="text-base text-justify leading-relaxed">
+                {" "}
+                Aliquam blandit leo lorem, nec vehicula mi accumsan posuere.
+                Duis condimentum ornare ante a posuere. Aliquam sem magna,
+                sodales quis congue vel, congue nec nisi. Aenean nec lorem et
+                dolor aliquet ullamcorper eu at sapien. Sed pretium mauris dui.
+                Phasellus mollis accumsan tempus. Proin laoreet hendrerit
+                ligula, id lacinia eros. Aenean consequat, felis eu malesuada
+                lobortis, augue nisl faucibus ligula, id viverra mi erat at
+                nisl. Pellentesque habitant morbi tristique senectus et netus et
+                malesuada fames ac turpis egestas. Maecenas ac aliquet eros.
+              </p>
+              <p className="mt-6 text-base text-justify leading-relaxed">
+                Aliquam a fringilla metus. Fusce ut tortor ut sem sagittis
+                convallis. Morbi et augue sed nisl fermentum dictum eget ut
+                nisl. Mauris ut dolor posuere augue porta lobortis nec sed
+                lacus. Curabitur dignissim ac lorem in tempor. Morbi id dui quis
+                ex malesuada mattis. Suspendisse potenti. Nulla euismod diam ut
+                risus mollis, id vehicula nulla dictum.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
