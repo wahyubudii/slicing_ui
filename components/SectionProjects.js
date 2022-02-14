5b2a7771bbafbd728cba6ff081275cc92aad56ec
wahyubudii <79882296+wahyubudii@users.noreply.github.com>
2022-02-14 17:29:32 +0700

    update project section

--- components/SectionProjects.js
+++ components/SectionProjects.js
@@ -1,3 +1,4 @@
+import ProjectItem from "./ProjectItem";
 import SectionParagraph from "./SectionParagraph";
 import SectionTitle from "./SectionTitle";
 
@@ -7,27 +8,35 @@ export default function SectionProjects() {
       <div className="container mx-auto">
         <SectionTitle>Projects</SectionTitle>
         <SectionParagraph>Beberapa proyek bikinan saya</SectionParagraph>
-        <div className="flex mt-4 flex-wrap">
-            <article className="mt-14 px-4 w-6/12 text-center">
-                <img src="/resources.png" className="rounded-lg"/>
-                <h3 className="mt-6 text-xl font-semibold">Hasten</h3>
-                <p className="text-lg mt-4 text-gray-500">Explorasi landing page</p>
-            </article>
-            <article className="mt-14 px-4 w-6/12 text-center">
-                <img src="/resources.png" className="rounded-lg"/>
-                <h3 className="mt-6 text-xl font-semibold">Hasten</h3>
-                <p className="text-lg mt-4 text-gray-500">Explorasi landing page</p>
-            </article>
-            <article className="mt-14 px-4 w-6/12 text-center">
-                <img src="/resources.png" className="rounded-lg"/>
-                <h3 className="mt-6 text-xl font-semibold">Hasten</h3>
-                <p className="text-lg mt-4 text-gray-500">Explorasi landing page</p>
-            </article>
-            <article className="mt-14 px-4 w-6/12 text-center">
-                <img src="/resources.png" className="rounded-lg"/>
-                <h3 className="mt-6 text-xl font-semibold">Hasten</h3>
-                <p className="text-lg mt-4 text-gray-500">Explorasi landing page</p>
-            </article>
+        <div className="flex mt-20 -mx-4 flex-wrap">
+          <div className="w-6/12 px-4 pb-8">
+            <ProjectItem 
+              name="Hasten"
+              description="Explorasi Landing Page"
+              image="/resources.png"
+            />
+          </div>
+          <div className="w-6/12 px-4 pb-8">
+            <ProjectItem 
+              name="Resources"
+              description="Explorasi Resources Page"
+              image="/resources.png"
+            />
+          </div>
+          <div className="w-6/12 px-4 pb-8">
+            <ProjectItem 
+              name="Hasten"
+              description="Explorasi Landing Page"
+              image="/resources.png"
+            />
+          </div>
+          <div className="w-6/12 px-4 pb-8">
+            <ProjectItem 
+              name="Resources"
+              description="Explorasi Resources Page"
+              image="/resources.png"
+            />
+          </div>
         </div>
       </div>
     </section>
