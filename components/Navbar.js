bd7a8ffe592cfeb964207914c0f68da4a3ac99c6
Wahyu Budi Utomo <79882296+wahyubudii@users.noreply.github.com>
2022-02-18 18:27:49 +0700

    all of it is done

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,11 @@ import Logo from "./Logo";
 import Nav from "./Nav";
 import Button from "./Button";
 
+import { useState } from "react";
+
 export default function NavBar() {
+  const [offcanvas, setOffcanvas] = useState(false)
+  
   return (
     <>
       <div className="flex items-center py-10">
@@ -18,11 +22,12 @@ export default function NavBar() {
           </Button>
         </div>
         <div className="w-9/12 md:hidden text-right">
-          <img src="/menu.svg" className="inline-block" />
+          <img src="/menu.svg" className="inline-block" onClick={() => setOffcanvas(true)}/>
         </div>
       </div>
 
-      <div className="fixed bg-white z-10 right-0 top-0 h-full w-full p-10 md:hidden">
+      <div className={`fixed bg-white z-10 top-0 h-full w-7/12 p-10 md:hidden transition-all ${offcanvas ? "right-0" : "-right-full"}`}>
+        <img src="/x.svg" className="w-8 absolute top-8 right-8" onClick={() => setOffcanvas(false)}/>
         <Nav scheme="dark" dir="vertical"/>
       </div>
     </>
